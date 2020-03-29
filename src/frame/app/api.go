e418139c5251e63b65eab4decd042b7423576ac6
imsjw <1324618209@qq.com>
2020-03-29 12:52:24 +0800

    修改oauth

--- src/frame/app/api.go
+++ src/frame/app/api.go
@@ -4,12 +4,14 @@ import (
 	"frame/config"
 	"frame/datasource"
 	"frame/log"
+	"frame/oauth"
 )
 
 func Init() {
 	log.Info("frame.app.Start 开始启动应用")
 	config.Init()
 	datasource.Init()
+	oauth.Init()
 }
 
 func Start() {
--- src/frame/oauth/api.go
+++ src/frame/oauth/api.go
@@ -2,5 +2,8 @@ package oauth
 
 func Init() {
 	initConfig()
+	if !configEnable {
+		return
+	}
 	initRouter()
 }
